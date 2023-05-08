c560981b909c5768579fde4d23a3444eed38cc6c
Potassium-chromate <112847018+Potassium-chromate@users.noreply.github.com>
2023-05-08 22:56:58 +0800

    Update Create_test_file.m

--- code and test data/Create_test_file.m
+++ code and test data/Create_test_file.m
@@ -5,11 +5,19 @@ load('Nevada.mat');
 X = reshape(X, size(X, 1)*size(X, 2), size(X, 3))';
 dim = size(X);
 
-%cols_to_masked = randperm(dim(2),int32(dim(2)*masked_num));
-cols_to_masked = 2:3:dim(2)-2;
+cols_to_masked = 2:2:dim(2)-2;
+
+%cols_to_masked = [];
+
 mask = ones(size(X));
 mask(:,cols_to_masked) = 0;
 
-X = X.*mask;
+cols_to_masked = 2:13:dim(2)-2;
+mask(:,cols_to_masked) = 0;
 
-save('test.mat', 'X');
+
+
+
+X_c = X.*mask;
+
+save('test.mat', 'X_c');
